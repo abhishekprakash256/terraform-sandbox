8becd99793c00b03b5aa77c253bb6bb3e44c46f3
abhishekprakash256 <abhishekprakash47@gmail.com>
2022-12-21 08:55:53 -0500

    test terraform

--- configuration_files/local/main.tf
+++ configuration_files/local/main.tf
@@ -1,4 +1,17 @@
 resource "local_file" "test" {
- filename = "/home/abhi/Programming/Terraform/test.txt"
- content = "This is test"    
+    filename = "/home/abhi/Programming/Terraform/test.txt"
+    content = "This is test"    
+}
+
+terraform {
+  required_providers {
+    installer = {
+      source = "shihanng/installer"
+      version = "0.3.0"
+    }
+  }
+}
+resource "installer_apt" "neofetch" {
+    name = "neofetch"
+  
 }
